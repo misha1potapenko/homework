1f1e6d7d9f35fccfd0d904516f2f6f69a7989866
misha1potapenko <misha1potapenko@yandex.ru>
2022-05-27 19:30:40 +0300

    firsthomework

---
+++ first_HW.py
@@ -0,0 +1,8 @@
+multiplication = 15 * 3
+division = 15 / 3
+division_of_the_whole = 15 // 2
+degree = 15 ** 2
+print(type(multiplication))
+print(type(division))
+print(type(division_of_the_whole))
+print(type(degree))
