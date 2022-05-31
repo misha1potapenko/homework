974a1afddf64cd3664df0bf90ef34e76cb34f0a1
misha1potapenko <misha1potapenko@yandex.ru>
2022-05-31 19:36:54 +0300

    first translate

---
+++ first_hw.py
@@ -0,0 +1,29 @@
+translate = {
+    'one': 'один',
+    'two': 'два',
+    'free': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять'
+}
+print(translate["six"])
+
+
+
+
+def num_translate(num):
+    print(translate[num])
+
+
+
+
+
+num_translate('six')
+num_translate('nine')
+num_translate('ten')
+num_translate('tenee')
+
